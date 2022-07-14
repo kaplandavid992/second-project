21214313534d8cfae4ae29f32f57faaf6f16da4d
kaplandavid992 <93373475+kaplandavid992@users.noreply.github.com>
2022-07-14 11:48:44 +0300

    Delete react-around-api-full directory

--- react-around-api-full/backend/helpers/files.js
+++
@@ -1,9 +0,0 @@
-const fsPromises = require('fs').promises;
-
-const getJsonFromFile = (filePath) => fsPromises.readFile(filePath, { encoding: 'utf8' })
-  .then((file) => JSON.parse(file))
-  .catch(() => {
-    throw new Error({ message: 'File is missing' });
-  });
-
-module.exports = { getJsonFromFile };
--- react-around-api-full/backend/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { registerAdmin, authAdmin } = require('../controllers/admin');
-
-router.post('/register', registerAdmin);
-router.post('/auth', authAdmin);
-
-module.exports = router;
-
-
